59b940b33e7fb8f4cb24a0ad8fae643137878fd3
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-03-13 14:12:54 -0400

    updated cors to allow for react app

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const cors = require('cors');
+
 
 const projectRouter = require('./data/routers/projectRouter.js');
 const actionRouter = require('./data/routers/actionRouter.js');
@@ -6,6 +8,7 @@ const actionRouter = require('./data/routers/actionRouter.js');
 const server = express();
 
 server.use(express.json());
+server.use(cors());
 
 server.get('/', (req, res) => {
     res.send("Node Api Sprint Challenge!");
