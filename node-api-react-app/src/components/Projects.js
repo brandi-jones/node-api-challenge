f2ce519c0ecbf3ab3114bf662a79bd14bcb83033
Brandi Jones <49703389+brandi-jones@users.noreply.github.com>
2020-03-13 15:11:37 -0400

    set up state in project.js to set up functionality to click on project cards to then show their actions associated with them

--- node-api-react-app/src/components/Projects.js
+++ node-api-react-app/src/components/Projects.js
@@ -1,13 +1,36 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 
 function Projects(props) {
 
+    const [actions, setActions] = useState([]);
+    const [ID, setID] = useState(0)
+    const [getActions, setGetActions] = useState(false);
+
+    useEffect(() => {
+
+        axios.get(`http://localhost:5000/api/projects/${ID}`)
+          .then(response => {
+            setActions(response.data.actions)
+          })
+          .catch(error => {
+            console.log(error)
+          })
+          
+    }, [ID])
+
+    function setButton(id) {
+        setGetActions(!getActions);
+        console.log(getActions)
+        console.log('this id', id)
+        setID(id);
+    }
 
     return (
         <>
             <h1>Projects</h1>
             {props.projects.map(project => (
-                <div className="projectCard">
+                <div className="projectCard" onClick={() => setButton(project.id)} >
                     <h2>{project.name}</h2>
                     <h3>{project.description}</h3>
                 </div>
